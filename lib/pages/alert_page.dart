9131c589f4605eab1cf7b30e875142cabed3b019
Nelson Bryan <201913582B@utea.edu.pe>
2022-10-31 12:56:28 -0500

    fin del proyecto

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -12,7 +12,7 @@ class AlertPage extends StatelessWidget {
           ),
           backgroundColor: Colors.white,
           content: Text(
-            "eres una perra >:v",
+            "sin ideas?",
           ),
           actions: [
             Row(
@@ -30,11 +30,172 @@ class AlertPage extends StatelessWidget {
                 ),
                 TextButton(
                   onPressed: () {},
-                  child: Text("largate"),
+                  child: Text("a e a"),
                 ),
               ],
             )
           ],
+        );
+      },
+    );
+  }
+
+  showMyAlert2(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FadeInImage(
+                  placeholder: AssetImage("assets/images/loading.gif"),
+                  image: NetworkImage(
+                    "https://images.pexels.com/photos/1926404/pexels-photo-1926404.jpeg",
+                  ),
+                  fit: BoxFit.cover,
+                  fadeInDuration: Duration(milliseconds: 2500),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Title(
+                  color: Colors.black,
+                  child: Text(
+                    "Blog post published",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "tihs blog post has been published . Team members"
+                  "will be able to edit this post and republish changes",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Confirm",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          );
+        });
+  }
+
+  showMyAlert3(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeInImage(
+                placeholder: AssetImage("assets/images/loading.gif"),
+                image: NetworkImage(
+                    "https://i0.wp.com/news.qoo-app.com/en/wp-content/uploads/sites/3/2022/08/More-Than-a-Married-Couple-But-Not-Lovers-Anime-Reveals-1st-PV-Main-Cast-and-October-Debut_cover.jpeg"),
+                fit: BoxFit.cover,
+                fadeInDuration: Duration(milliseconds: 2500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text("Your video has been uploaded"),
+              Text("you're video has finished uploading end is live"),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () {}, child: Text("Ingresa un correo")),
+                  TextButton(
+                    onPressed: () {},
+                    child: Card(
+                      child: Row(
+                        children: [
+                          Icon(Icons.share),
+                          Text(
+                            "Copy Link",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 6, 2, 243)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Skip",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Card(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        "Next ",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    color: Color.fromARGB(255, 87, 35, 146),
+                  ),
+                ],
+              ),
+            ],
+          ),
         );
       },
     );
@@ -56,7 +217,19 @@ class AlertPage extends StatelessWidget {
               onPressed: () {
                 showMyAlert(context);
               },
-              child: Text("alert!!"),
+              child: Text("primera alerta"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert2(context);
+              },
+              child: Text("Segunda alerta"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert3(context);
+              },
+              child: Text("Tercera alerta"),
             ),
           ],
         ),
